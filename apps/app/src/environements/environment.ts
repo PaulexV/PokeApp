e44d604fe56957578163dbaea781ce7fa06d09fa
Paulex <paulalexis.verrier@ynov.com>
2022-12-15 15:33:21 +0100

    feat(init): init project

---
+++ apps/app/src/environements/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'pokeapp-f1c97',
+    appId: '1:1037505311189:web:6a18d82bcd5689c9da0d5c',
+    storageBucket: 'pokeapp-f1c97.appspot.com',
+    locationId: 'europe-west3',
+    apiKey: 'AIzaSyA4NlcRt_9Ge3Ew4JdJNoY1J6Tywa-n3pI',
+    authDomain: 'pokeapp-f1c97.firebaseapp.com',
+    messagingSenderId: '1037505311189',
+  },};
