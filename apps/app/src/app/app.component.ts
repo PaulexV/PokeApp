fda653c921efce0fd38c35edf926129880bdcb9b
Astruum0 <arthurvella33@gmail.com>
2023-01-05 12:26:55 +0100

    feat(navbar)

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
+  imports: [NxWelcomeComponent, NavbarComponent],
   selector: 'poke-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
