9d5d4dba4dad72cd95574322e0af35ecaefbf576
Paulex <paulalexis.verrier@ynov.com>
2023-01-06 11:19:30 +0100

    feat(pages): init pokedex page

---
+++ apps/app/src/app/pages/pokedex/pokedex.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'poke-app-pokedex',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pokedex.component.html',
+  styleUrls: ['./pokedex.component.css']
+})
+export class PokedexComponent {
+
+}
