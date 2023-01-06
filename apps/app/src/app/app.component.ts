fa2f8a51642dae5a13a7f5951157e7ab3e47fd61
Astruum0 <arthurvella33@gmail.com>
2023-01-06 15:14:53 +0100

    feat(header)

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { HuntComponent } from './components/hunt/hunt.component';
-import { PokedexComponent } from './components/pokedex/pokedex.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HuntComponent } from './pages/hunt/hunt.component';
+import { PokedexComponent } from './pages/pokedex/pokedex.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { RouterModule } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @Component({
-  standalone: true,
-  imports: [RouterModule, 
-    NavbarComponent, 
-    HomeComponent, 
-    HuntComponent, 
-    PokedexComponent, 
-    ProfileComponent, 
-    LoginPageComponent],
-  selector: 'poke-app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+	standalone: true,
+	imports: [
+		RouterModule,
+		NavbarComponent,
+		HomeComponent,
+		HuntComponent,
+		PokedexComponent,
+		ProfileComponent,
+		LoginPageComponent,
+	],
+	selector: 'poke-app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 	title = 'app';
