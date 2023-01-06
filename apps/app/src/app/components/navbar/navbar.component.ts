536c6a3bb2ddbeae69828dbe69e511e2c5fd13c5
Paulex <paulalexis.verrier@ynov.com>
2023-01-06 16:24:28 +0100

    feat(navbar): do not display if not logged in

--- apps/app/src/app/components/navbar/navbar.component.ts
+++ apps/app/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
 	standalone: true,
@@ -8,4 +9,12 @@ import { RouterModule } from '@angular/router';
 	templateUrl: './navbar.component.html',
 	styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+	isLoggedIn$: Observable<boolean>;
+
+	constructor(private authService: AuthService) {}
+
+	ngOnInit() {
+		this.isLoggedIn$ = this.authService.isLoggedIn;
+	}
+}
