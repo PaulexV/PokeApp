0b9ebd8288d1ffce16d70dc3cc30dfc5fa31babb
Paulex <paulalexis.verrier@ynov.com>
2023-01-06 15:14:15 +0100

    feat(sigin): add sigin files

---
+++ apps/app/src/app/pages/login-pages/signin-page/signin-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'poke-app-signin-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './signin-page.component.html',
+  styleUrls: ['./signin-page.component.css']
+})
+export class SigninPageComponent {
+
+}
