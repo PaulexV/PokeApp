f96247d5d187dc362eccf18ea8a2f97f2ea2f8a2
Paulex <paulalexis.verrier@ynov.com>
2023-01-13 11:49:44 +0100

    feat(sec): add guards for logged and unloged users

--- apps/app/src/app/app.routes.ts
+++ apps/app/src/app/app.routes.ts
@@ -5,14 +5,16 @@ import { PokedexComponent } from './pages/pokedex/pokedex.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { SigninPageComponent } from './pages/login-pages/signin-page/signin-page.component';
 import { LoginPageComponent } from './pages/login-pages/login-page/login-page.component';
+import { UnloggedUsersGuard } from './services/unloggedUsersGuard.service';
+import { LoggedUsersGuard } from './services/loggedUsersGuard.service';
 
 export const appRoutes: Route[] = [
-	{ path: 'home', component: HomePageComponent },
-	{ path: 'pokedex', component: PokedexComponent },
-	{ path: 'play', component: HuntComponent },
-	{ path: 'profile', component: ProfileComponent },
-	{ path: 'register', component: SigninPageComponent },
-	{ path: 'login', component: LoginPageComponent },
-	{ path: '', component: HomePageComponent },
+	{ path: '', component: HomePageComponent, canActivate: [UnloggedUsersGuard] },
+	{ path: 'home', component: HomePageComponent, canActivate: [LoggedUsersGuard] },
+	{ path: 'pokedex', component: PokedexComponent, canActivate: [LoggedUsersGuard] },
+	{ path: 'play', component: HuntComponent, canActivate: [LoggedUsersGuard] },
+	{ path: 'profile', component: ProfileComponent, canActivate: [LoggedUsersGuard] },
+	{ path: 'register', component: SigninPageComponent, canActivate: [UnloggedUsersGuard] },
+	{ path: 'login', component: LoginPageComponent, canActivate: [UnloggedUsersGuard] },
 	{ path: '**', redirectTo: '' },
 ];
