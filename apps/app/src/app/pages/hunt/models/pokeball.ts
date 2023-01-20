94b72f50a094514d38040271740b1dc2fb8052a8
Astruum0 <arthurvella33@gmail.com>
2023-01-20 11:27:56 +0100

    fix: masterball model data

--- apps/app/src/app/pages/hunt/models/pokeball.ts
+++ apps/app/src/app/pages/hunt/models/pokeball.ts
@@ -2,5 +2,5 @@ export const ballsStats = {
     "pokeball": {catchrate: 10, cooldown: 5},
     "superball": {catchrate: 25, cooldown: 15},
     "ultraball": {catchrate: 50, cooldown: 60},
-    "masterball": {catchrate: 100, cooldown: 2881}
+    "masterball": {catchrate: 100, cooldown: 10080}
 }
