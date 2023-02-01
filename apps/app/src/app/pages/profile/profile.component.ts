cbed8f959868fb8652c2f9c080ba125d7d1c5909
paul-lautier <paul.lautier64@gmail.Com>
2023-02-01 20:06:17 +0100

    yo ^^

--- apps/app/src/app/pages/profile/profile.component.ts
+++ apps/app/src/app/pages/profile/profile.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
+import { HeaderComponent } from '../../components/header/header.component';
+
 
 @Component({
 	selector: 'poke-app-profile',
 	standalone: true,
-	imports: [CommonModule],
 	templateUrl: './profile.component.html',
 	styleUrls: ['./profile.component.css'],
+	imports: [HeaderComponent,CommonModule],
 })
 export class ProfileComponent {
 	isLoggedIn$: Observable<boolean> = this.auth.isLoggedIn$;
